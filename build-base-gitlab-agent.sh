95b2fda118720cd2986d20c9fa80b4d87049e7b1
Dmitry Ovcharenko <DOvcharenko@korusconsulting.ru>
2026-01-28 16:20:07 +0300

    registry-less gitlab-agent build

--- build-base-gitlab-agent.sh
+++ build-base-gitlab-agent.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD $DOCKER_REGISTRY_URL
+if [ -n "${DOCKER_LOGIN}" ] && [ -n "${DOCKER_PASSWORD}" ] && [ -n "${DOCKER_REGISTRY_URL}" ]; then
+    if ! docker login -u "${DOCKER_LOGIN}" -p "${DOCKER_PASSWORD}" "${DOCKER_REGISTRY_URL}"; then
+        echo "Docker login failed"
+        exit 1
+    fi
+else
+    echo "Skipping Docker login due to missing credentials"
+fi
 
 if [ $DOCKER_SYSTEM_PRUNE = 'true' ] ; then
     docker system prune -af
@@ -19,7 +26,7 @@ docker build \
     --build-arg BASE_IMAGE=ubuntu \
     --build-arg BASE_TAG=20.04 \
     --build-arg ONESCRIPT_PACKAGES="yard" \
-    -t $DOCKER_REGISTRY_URL/oscript-downloader:latest \
+    -t ${DOCKER_REGISTRY_URL:+"$DOCKER_REGISTRY_URL/"}oscript-downloader:latest \
 	-f oscript/Dockerfile \
     $last_arg
 
@@ -30,29 +37,34 @@ docker build \
     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
     --build-arg BASE_IMAGE=oscript-downloader \
     --build-arg BASE_TAG=latest \
-    -t $DOCKER_REGISTRY_URL/onec-client:$ONEC_VERSION \
+    -t ${DOCKER_REGISTRY_URL:+"$DOCKER_REGISTRY_URL/"}onec-client:$ONEC_VERSION \
     -f client/Dockerfile \
     $last_arg
 
-docker push $DOCKER_REGISTRY_URL/onec-client:$ONEC_VERSION
+if [[ -n "$DOCKER_REGISTRY_URL" ]]; then
+  docker push $DOCKER_REGISTRY_URL/onec-client:$ONEC_VERSION
+else
+  echo "DOCKER_REGISTRY_URL not set, skipping docker push."
+fi
 
 docker build \
-    --pull \
-    --build-arg ONEC_USERNAME=$ONEC_USERNAME \
-    --build-arg ONEC_PASSWORD=$ONEC_PASSWORD \
     --build-arg ONEC_VERSION=$ONEC_VERSION \
     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
-    -t $DOCKER_REGISTRY_URL/onec-client-vnc:$ONEC_VERSION \
+    -t ${DOCKER_REGISTRY_URL:+"$DOCKER_REGISTRY_URL/"}onec-client-vnc:$ONEC_VERSION \
     -f client-vnc/Dockerfile \
     $last_arg
 
-docker push $DOCKER_REGISTRY_URL/onec-client-vnc:$ONEC_VERSION
+if [[ -n "$DOCKER_REGISTRY_URL" ]]; then
+  docker push $DOCKER_REGISTRY_URL/onec-client-vnc:$ONEC_VERSION
+else
+  echo "DOCKER_REGISTRY_URL not set, skipping docker push."
+fi
 
 docker build \
     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
     --build-arg BASE_IMAGE=onec-client-vnc \
     --build-arg BASE_TAG=$ONEC_VERSION \
-    -t $DOCKER_REGISTRY_URL/onec-client-vnc-oscript:$ONEC_VERSION \
+    -t ${DOCKER_REGISTRY_URL:+"$DOCKER_REGISTRY_URL/"}onec-client-vnc-oscript:$ONEC_VERSION \
     -f oscript/Dockerfile \
     $last_arg
 
@@ -60,7 +72,7 @@ docker build \
     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
     --build-arg BASE_IMAGE=onec-client-vnc-oscript \
     --build-arg BASE_TAG=$ONEC_VERSION \
-    -t $DOCKER_REGISTRY_URL/onec-client-vnc-oscript-jdk:$ONEC_VERSION \
+    -t ${DOCKER_REGISTRY_URL:+"$DOCKER_REGISTRY_URL/"}onec-client-vnc-oscript-jdk:$ONEC_VERSION \
     -f jdk/Dockerfile \
     $last_arg
 
@@ -68,7 +80,7 @@ docker build \
     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
     --build-arg BASE_IMAGE=onec-client-vnc-oscript-jdk \
     --build-arg BASE_TAG=$ONEC_VERSION \
-    -t $DOCKER_REGISTRY_URL/onec-client-vnc-oscript-jdk-testutils:$ONEC_VERSION \
+    -t ${DOCKER_REGISTRY_URL:+"$DOCKER_REGISTRY_URL/"}onec-client-vnc-oscript-jdk-testutils:$ONEC_VERSION \
     -f test-utils/Dockerfile \
     $last_arg
 
@@ -80,4 +92,8 @@ docker build \
     -f gitlab-agent/Dockerfile \
     $last_arg
 
-docker push $DOCKER_REGISTRY_URL/base-gitlab-agent:$ONEC_VERSION
+if [[ -n "$DOCKER_REGISTRY_URL" ]]; then
+  docker push $DOCKER_REGISTRY_URL/base-gitlab-agent:$ONEC_VERSION
+else
+  echo "DOCKER_REGISTRY_URL not set, skipping docker push."
+fi
